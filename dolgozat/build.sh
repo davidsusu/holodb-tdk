85b4ba48a5929c6f68ad6d121b75cf7f973f0d92
Dávid Horváth <horvath@webarticum.hu>
2023-10-30 21:52:12 +0100

    Updated: 7 files changed, 280 insertions(+), 5125 deletions(-)
    M  dolgozat/build.sh
    D  dolgozat/src/diagram/distribution.svg
    D  dolgozat/src/diagram/findvalue.svg
    D  dolgozat/src/diagram/getvalue.svg
    M  dolgozat/src/diagram/simplearch.drawio
    D  dolgozat/src/diagram/simplearch.svg
    M  dolgozat/src/index.tex

--- dolgozat/build.sh
+++ dolgozat/build.sh
@@ -11,6 +11,12 @@ cp -R ./src/. ./build
 
 cd build
 
+ls diagram/ --color=never | egrep '^.*\.drawio$' | while IFS=' ' read -r diagramFilename; do
+    diagramSvgFilename="$( echo "${diagramFilename}" | sed -E 's/.drawio$/.svg/' )"
+    drawio -x -f svg -o "diagram/${diagramSvgFilename}" "diagram/${diagramFilename}"
+    inkscape "diagram/${diagramSvgFilename}" --export-text-to-path --export-overwrite
+done
+
 command="pdflatex --shell-escape -interaction=nonstopmode -jobname='${jobName}' '${inputFileName}'"
 
 eval "$command" | exit 0
