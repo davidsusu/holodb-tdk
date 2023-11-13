9efc923c68bdd7e8f71bb9982eb5a7f6fcb03b8d
Dávid Horváth <horvath@webarticum.hu>
2023-11-13 04:55:24 +0100

    Updated: 5 files changed, 257 insertions(+), 75 deletions(-)
    M  dolgozat/build.sh
    A  dolgozat/src/diagram/.$storage-api-uml.drawio.bkp
    A  dolgozat/src/diagram/basic-ifaces-uml.drawio
    A  dolgozat/src/diagram/storage-api-uml.drawio
    M  dolgozat/src/index.tex

--- dolgozat/build.sh
+++ dolgozat/build.sh
@@ -12,6 +12,9 @@ cp -R ./src/. ./build
 cd build/diagram
 
 ls ./ --color=never | egrep '^.*\.drawio$' | while IFS=' ' read -r diagramFilename; do
+    diagramPngFilename="$( echo "${diagramFilename}" | sed -E 's/.drawio$/.png/' )"
+    drawio -x -f png -o "${diagramPngFilename}" "${diagramFilename}"
+    
     diagramSvgFilename="$( echo "${diagramFilename}" | sed -E 's/.drawio$/.svg/' )"
     drawio -x -f svg -o "${diagramSvgFilename}" "${diagramFilename}"
     inkscape "${diagramSvgFilename}" --export-text-to-path --export-overwrite
