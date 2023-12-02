ec2f755e3057c8131de64edbd1417f02a2f76ef4
Dávid Horváth <horvath@webarticum.hu>
2023-12-02 01:58:30 +0100

    Updated: 4 files changed, 71 insertions(+), 24 deletions(-)
    A  dolgozat/check.sh
    A  dolgozat/install.sh
    D  dolgozat/src/.tdk-j.txt.kate-swp
    M  dolgozat/src/index.tex

---
+++ dolgozat/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+apt-get install \
+    texlive-latex-base texlive-latex-recommended texlive-lang-european \
+    texlive-latex-extra texlive-bibtex-extra texlive-fonts-extra \
+    texlive-luatex texlive-pictures texlive-pstricks texlive-science \
+    hunspell hunspell-tools hunspell-hu \
+    inkscape \
+;
+
+if ! command -v drawio &> /dev/null; then
+    downloadUrl='https://github.com/jgraph/drawio-desktop/releases/download/v22.1.2/drawio-amd64-22.1.2.deb'
+    outputFile="/tmp/$( uuidgen ).deb"
+    curl --location "${downloadUrl}" --output "${outputFile}"
+    dpkg -i "${outputFile}"
+    rm -f "${outputFile}"
+else
+    echo 'drawio is already installed'
+fi
