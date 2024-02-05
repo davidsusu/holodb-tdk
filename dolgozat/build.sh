7f18c6a797f2f621202cf644d7bdbc1753abe5b1
Dávid Horváth <horvath@webarticum.hu>
2024-02-05 02:15:32 +0100

    Updated: 2 files changed, 30 insertions(+), 11 deletions(-)
    M  dolgozat/build.sh
    M  dolgozat/src/index.tex

--- dolgozat/build.sh
+++ dolgozat/build.sh
@@ -3,6 +3,11 @@
 inputFileName='index.tex'
 outputFileName='dolgozat.pdf'
 
+mode="$1"
+if [ -z "$mode" ]; then
+    mode='final'
+fi
+
 baseName="$( echo "${inputFileName}" | sed 's/\.[^.]*$//' )"
 
 mkdir -p build
@@ -12,19 +17,25 @@ cp -R ./src/. ./build
 
 cd build/diagram
 
-ls ./ --color=never | egrep '^.*\.drawio$' | while IFS=' ' read -r diagramFilename; do
-    diagramPngFilename="$( echo "${diagramFilename}" | sed -E 's/.drawio$/.png/' )"
-    drawio --export --format png --scale 2.5 --output "${diagramPngFilename}" "${diagramFilename}"
-    optipng -o7 -zm1-9 "${diagramPngFilename}"
-    
-    diagramSvgFilename="$( echo "${diagramFilename}" | sed -E 's/.drawio$/.svg/' )"
-    drawio --export --format svg --output "${diagramSvgFilename}" "${diagramFilename}"
-    inkscape "${diagramSvgFilename}" --export-text-to-path --export-overwrite
-done
+if ! [ "${mode}" = 'draft' ]; then
+    ls ./ --color=never | egrep '^.*\.drawio$' | while IFS=' ' read -r diagramFilename; do
+        diagramPngFilename="$( echo "${diagramFilename}" | sed -E 's/.drawio$/.png/' )"
+        drawio --export --format png --scale 2.5 --output "${diagramPngFilename}" "${diagramFilename}"
+        optipng -o7 -zm1-9 "${diagramPngFilename}"
+        
+        diagramSvgFilename="$( echo "${diagramFilename}" | sed -E 's/.drawio$/.svg/' )"
+        drawio --export --format svg --output "${diagramSvgFilename}" "${diagramFilename}"
+        inkscape "${diagramSvgFilename}" --export-text-to-path --export-overwrite
+    done
+fi
 
 cd ..
 
-command="pdflatex --shell-escape -interaction=nonstopmode '${inputFileName}'"
+if [ "${mode}" = 'draft' ]; then
+    command="pdflatex --shell-escape -interaction=nonstopmode '\makeatletter\def\@classoptionslist{draft}\makeatother\input{${inputFileName}}'"
+else
+    command="pdflatex --shell-escape -interaction=nonstopmode '${inputFileName}'"
+fi
 
 eval "${command}"
 bibtex "${baseName}"
