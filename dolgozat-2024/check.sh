d09fa3e07b4e325b5db08fb5586c670fb551635d
Dávid Horváth <horvath@webarticum.hu>
2024-11-14 04:04:37 +0100

    Updated: 3 files changed, 138 insertions(+), 12 deletions(-)
    M  dolgozat-2024/check.sh
    M  dolgozat-2024/src/index.tex
    A  dolgozat-2024/src/spellignore

--- dolgozat-2024/check.sh
+++ dolgozat-2024/check.sh
@@ -7,22 +7,27 @@ wordOutFile="/tmp/latex-check-words-$( uuidgen )"
 hunChars='í|Í|ó|Ó|ö|Ö|ő|Ő|ú|Ú|ü|Ü|ű|Ű'
 
 cat "${latexFile}" |
-    grep -vE '\\(logo|includegraphics|includesvg|documentlang)\b' |
+    grep -vE '\\(logo|includegraphics|includesvg|documentlang|todo)\b' |
     sed -E '/\\begin\{(minted)\}/,/\\end\{minted\}/d' |
     sed -E 's/\$\-([a-zA-Z]|'"${hunChars}"'){1,7}\b/$/gi' |
+    sed -E 's/(mailto:)?[a-zA-Z0-9.]+@[a-zA-Z0-9.]+//g' |
+    sed -E 's/\\(texttt|url|href)\{[^{}\\]+\}//g' |
     detex -l |
     sed -E 's/\b(ld|stb|pl)\.//gi' |
     sed -E 's/\-([a-zA-Z]|'"${hunChars}"'){1,7}\b//gi' |
     sed -E 's/\[//' |
-    sed -E 's/[-.?!:,;]/ /gi' > "${plainOutFile}" \
+    sed -E 's/[-.?!:,;]/ /gi' \
+    > "${plainOutFile}" \
 ;
 hunspell -t -l -i 'UTF-8' -d 'hu_HU,en_US' "${plainOutFile}" |
     sort -u |
-    grep -vE '[0-9]|.[A-Z]' \
+    grep -vE '[0-9]|.[A-Z]' |
+    grep -vE "^'.*'\$" \
     > "${wordOutFile}" \
 ;
 
-cat "${wordOutFile}"
+cat "${wordOutFile}" | grep -vF -f 'src/spellignore'
+cat 'src/spellignore' | grep -vF -f "${wordOutFile}" | sed -E 's/^/- /'
 
 rm -f "${plainOutFile}"
 rm -f "${wordOutFile}"
