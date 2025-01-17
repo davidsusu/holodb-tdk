14f31fdfb2df8b52d1381308bc6568dc0bda5927
Dávid Horváth <horvath@webarticum.hu>
2025-01-17 19:34:45 +0100

    Updated: 2 files changed, 31 insertions(+), 12 deletions(-)
    M  dolgozat-2024/src/index.tex
    M  load-test/holodb-load-client/src/main/java/hu/webarticum/miniconnect/test/holodbloadclient/LoadMain.java

--- load-test/holodb-load-client/src/main/java/hu/webarticum/miniconnect/test/holodbloadclient/LoadMain.java
+++ load-test/holodb-load-client/src/main/java/hu/webarticum/miniconnect/test/holodbloadclient/LoadMain.java
@@ -2,8 +2,12 @@ package hu.webarticum.miniconnect.test.holodbloadclient;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Supplier;
 
+import hu.webarticum.miniconnect.api.MiniResult;
+import hu.webarticum.miniconnect.api.MiniResultSet;
 import hu.webarticum.miniconnect.api.MiniSession;
 import hu.webarticum.miniconnect.api.MiniSessionManager;
 import hu.webarticum.miniconnect.messenger.adapter.MessengerSessionManager;
@@ -15,7 +19,11 @@ public class LoadMain {
 
     private static int PORT = 3430;
     
-    private static final String SQL_PATTERN_1 = "SELECT * FROM employees WHERE id = ?";
+    private static int FULL_MILLIS = 500;
+    
+    private static int SLEEP_MILLIS = 10;
+    
+    private static final String SQL_PATTERN_1 = "SELECT 1"; // "SELECT * FROM employees WHERE id = ?";
     
     private static final String SQL_PATTERN_2 =
             "SELECT e.*, c.identifier FROM coupons c \n" +
@@ -30,22 +38,23 @@ public class LoadMain {
     
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Start: " + LocalDateTime.now().format(formatter));
+        
         Supplier<String> sqlSupplier1 = () -> SQL_PATTERN_1.replace("?", "" + ((int) (Math.random() * 100)));
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < 1; i++) {
             startNewClientThread(sqlSupplier1);
         }
-        
+        /*
         Supplier<String> sqlSupplier2 = () -> SQL_PATTERN_2.replace("?", "" + ((int) (Math.random() * 100)));
         for (int i = 0; i < 15; i++) {
             startNewClientThread(sqlSupplier2);
         }
-        /*
-        Supplier<String> sqlSupplier3 = () -> SQL_PATTERN_3;
-        for (int i = 0; i < 15; i++) {
-            startNewClientThread(sqlSupplier3);
-        }*/
         
-        Thread.sleep(10000);
+        Supplier<String> sqlSupplier3 = () -> SQL_PATTERN_3;
+        for (int i = 0; i < 1; i++) {
+            startNewClientThread(sqlSupplier3);
+        }
+        */
+        Thread.sleep(FULL_MILLIS);
         stopped = true;
     }
     
@@ -56,6 +65,7 @@ public class LoadMain {
     private static void runClient(Supplier<String> sqlSupplier) {
         long threadId = Thread.currentThread().getId();
         System.out.println("Thread (" + threadId + ") started at " + LocalDateTime.now().format(formatter));
+        List<Long> times = new ArrayList<>(200);
         int i = 0;
         try (ClientMessenger clientMessenger = new ClientMessenger(HOST, PORT)) {
             MiniSessionManager sessionManager = new MessengerSessionManager(clientMessenger);
@@ -65,16 +75,25 @@ public class LoadMain {
                     String sql = sqlSupplier.get();
                     i++;
                     try {
-                        session.execute(sql).resultSet().close();;
-                        Thread.sleep(200);
+                        long start = System.currentTimeMillis();
+                        MiniResult result = session.execute(sql);
+                        try (MiniResultSet resultSet = result.resultSet()) {
+                            while (resultSet.fetch() != null) {
+                                // nothing to do
+                            }
+                        }
+                        long end = System.currentTimeMillis();
+                        Thread.sleep(SLEEP_MILLIS);
+                        times.add(end - start);
                     } catch (Exception e) {
                         e.printStackTrace();
                         break;
                     }
+                    System.out.println();
                 }
             }
         }
-        System.out.println("Thread (" + threadId + ") finished at " + LocalDateTime.now().format(formatter) + " :: count: " + i);
+        System.out.println("Thread (" + threadId + ") finished at " + LocalDateTime.now().format(formatter) + " :: count: " + i + ", times: " + times);
     }
 
 }
